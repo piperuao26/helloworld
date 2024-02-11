28e47f6c9f9d22b795c7e01e4e3630af807b0bc9
Andres Rua <piperuao26@gmail.com>
2024-02-10 19:11:56 -0500

    comit 2

--- pages/views.py
+++ pages/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from django.http import HttpResponse 
+from django.views.generic import TemplateView
 # Create your views here.
-def homePageView(request): 
-    return HttpResponse ('Valencia badman')
+class homePageView(TemplateView):
+    template_name = 'home.html'
