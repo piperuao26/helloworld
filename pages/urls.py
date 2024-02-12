fc0b09a6bfefcafe5efa4881f2df990ddc5755eb
Andres Rua <piperuao26@gmail.com>
2024-02-12 14:50:51 -0500

    Productos y creacion de ellos mismos, Falta acomodar la web a mascotas porque si :p

--- pages/urls.py
+++ pages/urls.py
@@ -1,7 +1,12 @@
 from django.urls import path
-from .views import homePageView, AboutPageView
+from .views import homePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView
 
 urlpatterns = [
     path("", homePageView.as_view(), name='home'),
     path('about/', AboutPageView.as_view(), name='about'),
+    path('products/', ProductIndexView.as_view(), name='index'),
+    path('products/create', ProductCreateView.as_view(), name='form'),
+    path('products/<str:id>', ProductShowView.as_view(), name='show'),
+
+
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,8 +1,71 @@
 from django.shortcuts import render
 from django.views.generic import TemplateView
+from django.views import View
+from django import forms
+from django.shortcuts import render, redirect
+
+
 # Create your views here.
 class homePageView(TemplateView):
     template_name = 'home.html'
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
+
+class Product:
+    products = [
+        {"id":"1", "name":"TV", "description":"Best TV"},
+        {"id":"2", "name":"iPhone", "description":"Best iPhone"},
+        {"id":"3", "name":"Chromecast", "description":"Best Chromecast"},
+        {"id":"4", "name":"Glasses", "description":"Best Glasses"}
+    ]
+
+class ProductIndexView(View):
+    template_name = 'products/index.html'
+
+    def get(self, request):
+        viewData = {}
+        viewData["title"] = "Products - Online Store"
+        viewData["subtitle"] =  "List of products"
+        viewData["products"] = Product.products
+
+        return render(request, self.template_name, viewData)
+
+class ProductShowView(View):
+    template_name = 'products/show.html'
+
+
+    def get(self, request, id):
+        viewData = {}
+        product = Product.products[int(id)-1]
+        viewData["title"] = product["name"] + " - Online Store"
+        viewData["subtitle"] =  product["name"] + " - Product information"
+        viewData["product"] = product
+
+        return render(request, self.template_name, viewData)
+
+class ProductForm(forms.Form):
+    name = forms.CharField(required=True)
+    price = forms.FloatField(required=True)
+
+    
+class ProductCreateView(View):
+    template_name = 'products/create.html'
+
+    def get(self, request):
+        form = ProductForm()
+        viewData = {}
+        viewData["title"] = "Create product"
+        viewData["form"] = form
+        return render(request, self.template_name, viewData)
+
+    def post(self, request):
+        form = ProductForm(request.POST)
+        if form.is_valid():
+            
+            return redirect(form) 
+        else:
+            viewData = {}
+            viewData["title"] = "Create product"
+            viewData["form"] = form
+            return render(request, self.template_name, viewData)
 
