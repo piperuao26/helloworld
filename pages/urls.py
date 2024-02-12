8f770181b1b370b7d487d7d543fc83db50fd8b7f
Andres Rua <piperuao26@gmail.com>
2024-02-12 14:34:12 -0500

    Commit about and changes inside

--- pages/urls.py
+++ pages/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path
-from .views import homePageView
+from .views import homePageView, AboutPageView
 
 urlpatterns = [
     path("", homePageView.as_view(), name='home'),
+    path('about/', AboutPageView.as_view(), name='about'),
 ]
--- pages/views.py
+++ pages/views.py
@@ -3,4 +3,6 @@ from django.views.generic import TemplateView
 # Create your views here.
 class homePageView(TemplateView):
     template_name = 'home.html'
+class AboutPageView(TemplateView):
+    template_name = 'pages/about.html'
 
