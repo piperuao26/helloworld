7c5b6161ea44ec40de1e6a9126922187cd950e9f
Andres Rua <piperuao26@gmail.com>
2024-02-23 13:20:54 -0500

    comit

--- pages/urls.py
+++ pages/urls.py
@@ -1,5 +1,5 @@
 from django.urls import path
-from .views import homePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView
+from .views import homePageView, AboutPageView, ProductIndexView, ProductShowView, ProductCreateView,CartRemoveAllView,CartView
 
 urlpatterns = [
     path("", homePageView.as_view(), name='home'),
@@ -7,6 +7,9 @@ urlpatterns = [
     path('products/', ProductIndexView.as_view(), name='index'),
     path('products/create', ProductCreateView.as_view(), name='form'),
     path('products/<str:id>', ProductShowView.as_view(), name='show'),
+    path('cart/', CartView.as_view(), name='cart_index'),
+    path('cart/add/<str:product_id>', CartView.as_view(), name='cart_add'),
+    path('cart/removeAll', CartRemoveAllView.as_view(), name='cart_removeAll'),
 
 
 ]
--- pages/views.py
+++ pages/views.py
@@ -10,10 +10,14 @@ from .models import Product
 
 
 # Create your views here.
+
 class homePageView(TemplateView):
     template_name = 'home.html'
+
+
 class AboutPageView(TemplateView):
     template_name = 'pages/about.html'
+
 
 class ProductIndexView(View):
     template_name = 'products/index.html'
@@ -25,6 +29,7 @@ class ProductIndexView(View):
         viewData["products"] = Product.objects.all()
 
         return render(request, self.template_name, viewData)
+    
 
 class ProductShowView(View):
     template_name = 'products/show.html'
@@ -57,7 +62,7 @@ class ProductForm(forms.ModelForm):
         fields = ['name', 'price']
 
 
-    
+
 class ProductCreateView(View):
     template_name = 'products/create.html'
 
@@ -79,6 +84,7 @@ class ProductCreateView(View):
             viewData["form"] = form
             return render(request, self.template_name, viewData)
 
+
 class ProductListView(ListView):
     model = Product
     template_name = 'product_list.html'
@@ -90,3 +96,49 @@ class ProductListView(ListView):
         context['subtitle'] = 'List of products'
         return context   
 
+
+class CartView(View):
+    template_name = 'cart/index.html'
+    
+    def get(self, request):
+        # Simulated database for products
+        products = {}
+        products[121] = {'name': 'Tv samsung', 'price': '1000'}
+        products[11] = {'name': 'Iphone', 'price': '2000'}
+
+        # Get cart products from session
+        cart_products = {}
+        cart_product_data = request.session.get('cart_product_data', {})
+
+        for key, product in products.items():
+            if str(key) in cart_product_data.keys():
+                cart_products[key] = product
+
+        # Prepare data for the view
+        view_data = {
+            'title': 'Cart - Online Store',
+            'subtitle': 'Shopping Cart',
+            'products': products,
+            'cart_products': cart_products
+        }
+
+        return render(request, self.template_name, view_data)
+
+    def post(self, request, product_id):
+        # Get cart products from session and add the new product
+        cart_product_data = request.session.get('cart_product_data', {})
+        cart_product_data[product_id] = product_id
+        request.session['cart_product_data'] = cart_product_data
+
+        return redirect('cart_index')
+
+
+class CartRemoveAllView(View):
+    def post(self, request):
+        # Remove all products from cart in session
+        if 'cart_product_data' in request.session:
+            del request.session['cart_product_data']
+
+        return redirect('cart_index')
+    
+    
